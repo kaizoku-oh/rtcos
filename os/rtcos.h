7de286c1e4b4955512ca3fc2e2e915a956825069
kaizoku-oh <garssallaoui.bayrem@gmail.com>
2021-04-10 10:43:23 +0100

    adding comments

--- os/rtcos.h
+++ os/rtcos.h
@@ -1,28 +1,57 @@
+/* 
+ **************************************************************************************************
+ *
+ * @file    : rtcos.h
+ * @author  : 
+ * @version : 1.0
+ * @date    : April 2021
+ * @brief   : RTCOS header file
+ * 
+ **************************************************************************************************
+ * 
+ * @project  : {Generic}
+ * @board    : {Generic}
+ * @target   : {Generic}
+ * @compiler : {Generic}
+ * 
+ **************************************************************************************************
+ *
+ */
+
 #ifndef RTCOS_H
 #define RTCOS_H
 
+/*-----------------------------------------------------------------------------------------------*/
+/* Includes                                                                                      */
+/*-----------------------------------------------------------------------------------------------*/
 #include "port.h"
 
+/*-----------------------------------------------------------------------------------------------*/
+/* Defines                                                                                       */
+/*-----------------------------------------------------------------------------------------------*/
 #define _NIL                                     0x00000000uL
-#define _TRUE                                    (_BOOL)(1 == 1)
-#define _FALSE                                   (_BOOL)(0 == 1)
+#define _TRUE                                    (_bool)(1 == 1)
+#define _FALSE                                   (_bool)(0 == 1)
 
-typedef char                                     _CHAR;
-typedef unsigned char                            _BOOL;
-typedef unsigned char                            _U08;
-typedef signed char                              _S08;
-typedef unsigned short                           _U16;
-typedef signed short                             _S16;
-typedef unsigned long                            _U32;
-typedef signed long                              _S32;
-typedef unsigned long long                       _U64;
-typedef signed long long                         _S64;
+/*-----------------------------------------------------------------------------------------------*/
+/* Types                                                                                         */
+/*-----------------------------------------------------------------------------------------------*/
+/** Generic types */
+typedef char                                     _char;
+typedef unsigned char                            _bool;
+typedef unsigned char                            _u08;
+typedef signed char                              _s08;
+typedef unsigned short                           _u16;
+typedef signed short                             _s16;
+typedef unsigned long                            _u32;
+typedef signed long                              _s32;
+typedef unsigned long long                       _u64;
+typedef signed long long                         _s64;
 typedef void (*pFunction)(void);
-
+/** RTCOS related types */
 typedef void (*pf_idle_handler_t)(void);
 typedef void (*pf_timer_cb_t)(void const *);
-typedef _U32 (*pf_task_handler_t)(_U32, _U08, _U32);
-
+typedef _u32 (*pf_task_handler_t)(_u32, _u08, _u32);
 typedef enum
 {
   RTCOS_ERR_NONE             = 0,
@@ -37,28 +66,30 @@ typedef enum
   RTCOS_ERR_MSG_EMPTY        = -9,
   RTCOS_ERR_ARG              = -10,
 }rtcos_status_t;
-
 typedef enum
 {
   RTCOS_TIMER_PERIODIC       = 0,
   RTCOS_TIMER_ONE_SHOT,
 }rtcos_timer_type_t;
 
+/*-----------------------------------------------------------------------------------------------*/
+/* Functions                                                                                     */
+/*-----------------------------------------------------------------------------------------------*/
 void rtcos_init(void);
 void rtcos_run(void);
-void rtcos_delay(_U32);
+void rtcos_delay(_u32);
 void rtcos_update_tick(void);
-void rtcos_set_tick_count(_U32);
-_U32 rtcos_get_tick_count(void);
-_U08 rtcos_create_timer(rtcos_timer_type_t, pf_timer_cb_t, void *);
-_BOOL rtcos_timer_expired(_U08);
-rtcos_status_t rtcos_start_timer(_U08, _U32);
-rtcos_status_t rtcos_stop_timer(_U08);
-rtcos_status_t rtcos_register_task_handler(pf_task_handler_t, _U08, _U32);
+void rtcos_set_tick_count(_u32);
+_u32 rtcos_get_tick_count(void);
+_u08 rtcos_create_timer(rtcos_timer_type_t, pf_timer_cb_t, void *);
+_bool rtcos_timer_expired(_u08);
+rtcos_status_t rtcos_start_timer(_u08, _u32);
+rtcos_status_t rtcos_stop_timer(_u08);
+rtcos_status_t rtcos_register_task_handler(pf_task_handler_t, _u08, _u32);
 rtcos_status_t rtcos_register_idle_handler(pf_idle_handler_t);
-rtcos_status_t rtcos_send_event(_U08, _U32, _U32, _BOOL);
-rtcos_status_t rtcos_clear_event(_U08, _U32);
-rtcos_status_t rtcos_send_message(_U08, void *);
+rtcos_status_t rtcos_send_event(_u08, _u32, _u32, _bool);
+rtcos_status_t rtcos_clear_event(_u08, _u32);
+rtcos_status_t rtcos_send_message(_u08, void *);
 rtcos_status_t rtcos_get_message(void **);
 
 #endif /* RTCOS_H */
