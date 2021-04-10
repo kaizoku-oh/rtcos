c02ca7a40b80700f2a7c53e6d78c9be5d03d4dfc
kaizoku-oh <garssallaoui.bayrem@gmail.com>
2021-04-10 23:00:50 +0100

    doxygen

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # RTC OS
 
+<!-- ![](https://github.com/<OWNER>/<REPOSITORY>/workflows/<WORKFLOW_NAME>/badge.svg) -->
+![GitHub Build workflow status](https://github.com/kaizoku-oh/rtcos/workflows/main/badge.svg)
 ![GitHub release](https://img.shields.io/github/v/release/kaizoku-oh/rtcos)
 ![GitHub issues](https://img.shields.io/github/issues/kaizoku-oh/rtcos)
 ![GitHub top language](https://img.shields.io/github/languages/top/kaizoku-oh/rtcos)
 ![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)
 
-is an event-driven RTC (Run To Completion) scheduler dedicated to ultra constrained devices.
+is an event-driven RTC (Run To Completion) scheduler dedicated to ultra constrained IoT devices.
 
 RTC OS is written in pure C and doesn't need any thirdparty library or dependency to work.
 
--- examples/linux/RTCOSConfig.h
+++ examples/linux/RTCOSConfig.h
@@ -1,24 +1,12 @@
-/* 
- **************************************************************************************************
- *
- * @file    : RTCOSConfig.h
- * @author  : Bayrem GHARSELLAOUI
- * @version : 1.0
- * @date    : April 2021
- * @brief   : RTCOS user configuration example used to overrite os default configuration
- * 
- **************************************************************************************************
- */
-
 #ifndef RTCOS_CONFIG_H
 #define RTCOS_CONFIG_H
 
-/* #define RTCOS_MAX_TASKS_COUNT                    2 */
-/* #define RTCOS_MAX_FUTURE_EVENTS_COUNT            2 */
-/* #define RTCOS_MAX_MESSAGES_COUNT                 2 */
-/* #define RTCOS_MAX_TIMERS_COUNT                   2 */
+#define RTCOS_MAX_TASKS_COUNT                    2
+#define RTCOS_MAX_FUTURE_EVENTS_COUNT            2
+#define RTCOS_MAX_MESSAGES_COUNT                 2
+#define RTCOS_MAX_TIMERS_COUNT                   2
 
-/* #define ENTER_CRITICAL_SECTION() */
-/* #define EXIT_CRITICAL_SECTION() */
+#define ENTER_CRITICAL_SECTION()
+#define EXIT_CRITICAL_SECTION()
 
 #endif /* RTCOS_CONFIG_H */
